5f850bb17399bcdd99ee72b894a1fb27964c848a
mohamed3610 <muhammad.mazen4@gmail.com>
2025-01-24 09:33:31 +0200

    forms.py

--- Classroom/Quiz/forms.py
+++ Classroom/Quiz/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 
 class EssaySubmissionForm(forms.Form):
-    images = forms.ImageField(
-        widget=forms.ClearableFileInput(attrs={'multiple': True}),
-        label="Upload images of your essay"
+    images = forms.FileField(
+        widget=forms.FileInput(attrs={
+            'multiple': True,
+            'accept': 'image/*'  # Optional: restrict to image files
+        }),
+        label="Upload images of your essay",
+        help_text="Select multiple images of your handwritten essay"
     )
