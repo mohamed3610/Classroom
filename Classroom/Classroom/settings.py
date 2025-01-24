c241997d525a025cfe876f31dfd2b17983f950d9
mohamed3610 <muhammad.mazen4@gmail.com>
2025-01-24 03:18:54 +0200

    hhh

--- Classroom/Classroom/settings.py
+++ Classroom/Classroom/settings.py
@@ -29,7 +29,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Directory where media files are 
 SECRET_KEY = 'django-insecure-=^@gc0gv0hx1(2wa7k1rn_rr37+an2fmls$f&h%70w7mkvax+&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mohamedwafa.pythonanywhere.com']
 
