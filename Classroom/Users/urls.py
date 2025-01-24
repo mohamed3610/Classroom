cb08a0c298b3ec3ca7bad2c6afd89c35cf7c5f87
mohamed3610 <muhammad.mazen4@gmail.com>
2025-01-24 04:52:14 +0200

    huiygutfurc

--- Classroom/Users/urls.py
+++ Classroom/Users/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views as auth_views
 from .views import signup_view , application_under_review , handle_upload , device_limit_reached
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('accounts/signup/', signup_view, name='signup'),
     path('application-under-review/', application_under_review, name='application_under_review'),
     path('upload/', handle_upload, name='handle_upload'),
