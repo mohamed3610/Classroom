b00f53a3925789ccdc3c519f46458dc74eeb90d5
mohamed3610 <muhammad.mazen4@gmail.com>
2025-01-24 10:36:22 +0200

    Quiz admin app

--- Classroom/Quiz/admin.py
+++ Classroom/Quiz/admin.py
@@ -1,16 +1,31 @@
-# from django.contrib import admin
-# from .models import WritingQuiz , WritingSubmission
-# from .models import StudentProfile
+from django.contrib import admin
+from .models import Quiz, Submission
 
-# @admin.register(StudentProfile)
-# class StudentProfileAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'approved')
-#     actions = ['approve_students']
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('title', 'group', 'created_at', 'due_date', 'is_published', 'is_active')
+    list_filter = ('group', 'is_published', 'created_at')
+    search_fields = ('title', 'description', 'instructions')
+    date_hierarchy = 'created_at'
+    ordering = ('-created_at',)
 
-#     def approve_students(self, request, queryset):
-#         queryset.update(approved=True)
-#         self.message_user(request, "Selected students have been approved.")
-#     approve_students.short_description = "Approve selected students"
+    def is_active(self, obj):
+        return obj.is_active()
+    is_active.boolean = True  # Display as a boolean icon (checkmark/cross)
+    is_active.short_description = 'Is Active'  # Column header
 
-# admin.site.register(WritingQuiz)
-# admin.site.register(WritingSubmission)
+@admin.register(Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ('student', 'quiz', 'submitted_at', 'grade', 'is_graded')
+    list_filter = ('quiz', 'is_graded', 'submitted_at')
+    search_fields = ('student__user__username', 'quiz__title', 'extracted_text')
+    date_hierarchy = 'submitted_at'
+    ordering = ('-submitted_at',)
+    readonly_fields = ('submitted_at',)  # Prevent editing of submission time
+
+    # Custom action to mark submissions as graded
+    actions = ['mark_as_graded']
+
+    def mark_as_graded(self, request, queryset):
+        queryset.update(is_graded=True)
+    mark_as_graded.short_description = "Mark selected submissions as graded"
